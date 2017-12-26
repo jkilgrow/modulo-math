39527eb6a12cd40b8dd1ab056f4076cd956d2e14
jkilgrow <jkilgrow@gmail.com>
2017-12-26 12:14:34 -0600

    initial code check-in

---
+++ src/main/goofy/stuff/NonModulo.java
@@ -0,0 +1,58 @@
+package goofy.stuff;
+
+import java.util.ArrayList;
+
+public class NonModulo
+{
+	public int[] findMultiplesWithMod(int min, int max)
+	{
+		ArrayList<Integer> multiples = new ArrayList<Integer>();
+
+		for (int i = min; i <= max; i++)
+		{
+			if ( (i % 3 == 0) || (i % 5 == 0) )
+			{
+				multiples.add(i);
+			}
+		}
+		
+		return convertIntArrListToArr(multiples);
+	}
+	
+	public int[] findMultiplesWithoutMod(int min, int max)
+	{
+		
+		ArrayList<Integer> multiples = new ArrayList<Integer>();
+
+		for (int i = min; i <= max; i++)
+		{
+			
+			if ( (calcRemainder(i, 3) == 0) || (calcRemainder(i, 5) == 0) )
+			{
+				multiples.add(i);
+			}
+		}
+
+		return convertIntArrListToArr(multiples);
+	}
+	
+	public int calcRemainder(int a, int b) //  a%b
+	{
+		int d = Math.floorMod(a, b);
+		return d;
+	}
+	
+	public int[] convertIntArrListToArr(final ArrayList<Integer> multiples)
+	{
+		int sizeMultArr = multiples.size();
+		
+		int[] multArr = new int[sizeMultArr];
+		
+		for (int i = 0; i < sizeMultArr; i++)
+		{
+			multArr[i] = multiples.get(i);
+		}
+		
+		return multArr;
+	}
+}
---
+++ src/test/goofy/stuff/TestNonModulo.java
@@ -0,0 +1,51 @@
+package goofy.stuff;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class TestNonModulo
+{
+	
+	@Test
+	void testFindMultiplesWithMod()
+	{
+		NonModulo noMo = new NonModulo();
+		int [] multiples = noMo.findMultiplesWithMod(1, 16); // 3, 5, 6, 9, 10, 12, 15
+		
+		for (int mult : multiples)
+		{
+			System.out.println("multiples by modulo: " + mult);
+		}
+	}
+	
+	@Test
+	void testFindMultiplesWithoutMod()
+	{
+		NonModulo noMo = new NonModulo();
+		int[] multiples = noMo.findMultiplesWithoutMod(1, 16); // 3, 5, 6, 9, 10, 12, 15
+		
+		for (int mult : multiples)
+		{
+			System.out.println("multiples by multiplication: " + mult);
+		}
+	}
+
+	@Test
+	void testConvertIntArrListToArr()
+	{
+		NonModulo noMo = new NonModulo();
+		
+		ArrayList<Integer> multiples = new ArrayList<Integer>();
+		multiples.add(1);
+		multiples.add(2);
+		multiples.add(3);
+
+		int[] mods = noMo.convertIntArrListToArr(multiples);
+		
+		assertEquals(3, mods.length);
+	}
+
+}
