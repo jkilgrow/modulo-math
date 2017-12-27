95c4d6c476aaac6671bcec0c2a337c26c4a266b3
jkilgrow <jkilgrow@gmail.com>
2017-12-27 09:57:25 -0600

    fixing project structure

---
+++ src/main/java/goofy/stuff/modulomath/ModuloMathApplication.java
@@ -0,0 +1,12 @@
+package goofy.stuff.modulomath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloMathApplication.class, args);
+	}
+}
